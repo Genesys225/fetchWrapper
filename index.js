a5bd754416b1409ee81bbe49a7e7e285cc87ff8e
gene <eugene@tohen-media.com>
2021-01-02 00:16:36 +0200

    full xml tag names

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const { FetchWrap } = require('./lib/fetchWrapper');
 const fetch = require('node-fetch');
 // fetch('https://www.crcind.com/csp/samples/SOAP.Demo.CLS?WSDL=1').then(console.log)
 async function run() {
-  const res = await new SOAPClient('https://www.crcind.com/csp/samples/SOAP.Demo.CLS').get('?WSDL=1');
-  console.log(res.definitions.types.schema.element);
+	const res = await new SOAPClient(
+		'https://www.crcind.com/csp/samples/SOAP.Demo.CLS'
+	).get('?WSDL=1');
+	console.log(res.definitions.types['s:schema']['s:element']);
 }
 run();
 
