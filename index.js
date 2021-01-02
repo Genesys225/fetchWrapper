3ecc80fbdcdd13d1e0d14de44cd02fa25e00b35d
gene <eugene@tohen-media.com>
2021-01-02 14:37:13 +0200

    almostt there - missing multy arrays, float atts + errors

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const { SOAPClient } = require('./lib/soap-client');
 const { RestClient, rest } = require('./lib/rest-client');
 const { FetchWrap } = require('./lib/fetchWrapper');
 const fetch = require('node-fetch');
-// fetch('https://www.crcind.com/csp/samples/SOAP.Demo.CLS?WSDL=1').then(console.log)
 async function run() {
 	const res = await new SOAPClient(
 		'https://www.crcind.com/csp/samples/SOAP.Demo.CLS'
