3f1879dc47b7956b272dff6bf82fec3f0176e4a9
gene <eugene@tohen-media.com>
2021-01-03 02:37:48 +0200

    moving to composite

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { SOAPClient } = require('./lib/soap-client');
 const { RestClient, rest } = require('./lib/rest-client');
 const { FetchWrap } = require('./lib/fetchWrapper');
-const fetch = require('node-fetch');
 async function run() {
 	const res = await new SOAPClient(
 		'https://www.crcind.com/csp/samples/SOAP.Demo.CLS'
