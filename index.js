17cbec5d109b6f1937a0d710f7010c61a2b27d77
gene <eugene@tohen-media.com>
2021-01-16 23:21:51 +0200

    start work on tree shaking and prevent bundle size in browser

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-const { SOAPClient } = require('./lib/soap-client');
-const { RestClient, rest } = require('./lib/rest-client');
-const { FetchWrap } = require('./lib/fetchWrapper');
-async function run() {
-	const res = new SOAPClient(
-		'https://www.crcind.com/csp/samples/SOAP.Demo.CLS'
-	);
-	const json = await res.get('?WSDL=1');
-	const Json2Xml = new res.Json2Xml(json);
-	console.log(res.serializer.serializeToString(Json2Xml.xmlDoc));
-}
-run();
-
-module.exports = { FetchWrap, RestClient, SOAPClient, rest };
+export { SOAPClient } from './lib/soap-client';
+export { RestClient, rest } from './lib/rest-client';
+export { FetchWrap } from './lib/fetchWrapper';
